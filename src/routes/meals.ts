de68c39e8861329d4f1a24184f55ea1298371dc9
efsiqueira <edu.fss14@gmail.com>
2026-02-15 00:06:45 -0300

    ✨ feat(meals): add routes

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,152 @@
+import z from "zod"
+import { prisma } from "@/lib/prisma"
 import type { FastifyInstance } from "fastify"
+import { checkSessionIdExists } from "@/middlewares/check-session-id-exists"
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post('/', async (request) => {
-    console.log(request.body)
+  app.post('/', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.string(),
+      isOnDiet: z.boolean()
+    })
+
+    const { name, description, date, isOnDiet } = createMealBodySchema.parse(
+      request.body
+    )
+
+    const currentUserId = String(request.user?.id)
+
+    await prisma.meal.create({
+      data: {
+        name,
+        description,
+        date: new Date(date),
+        isOnDiet,
+        userId: currentUserId
+      }
+    })
+
+    reply.status(201).send()
+  })
+
+  app.put('/:id', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const updateMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.string(),
+      isOnDiet: z.boolean()
+    })
+
+    const updateMealParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = updateMealParamsSchema.parse(request.params)
+
+    await prisma.meal.findFirstOrThrow({
+      where: {
+        id,
+        userId: String(request.user?.id)
+      }
+    })
+
+    const { name, description, date, isOnDiet } = updateMealBodySchema.parse(
+      request.body
+    )
+
+    await prisma.meal.update({
+      where: {
+        id,
+        userId: String(request.user?.id)
+      },
+      data: {
+        name,
+        description,
+        date: new Date(date),
+        isOnDiet
+      }
+    })
+
+    reply.status(204).send()
+  })
+
+  app.delete('/:id', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const deleteMealParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = deleteMealParamsSchema.parse(request.params)
+
+    await prisma.meal.findFirstOrThrow({
+      where: {
+        id,
+        userId: String(request.user?.id)
+      }
+    })
+
+    await prisma.meal.delete({
+      where: {
+        id,
+        userId: String(request.user?.id)
+      }
+    })
+
+    reply.status(204).send()
+  })
+
+  app.get('/metrics', {
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+    const totalMealsRegistered = await prisma.meal.findMany({
+      where: {
+        userId: String(request.user?.id)
+      },
+      orderBy: {
+        date: 'desc'
+      }
+    })
+
+    const totalMealsOnDiet = await prisma.meal.count({
+      where: {
+        userId: String(request.user?.id),
+        isOnDiet: true
+      }
+    })
+
+    const totalMealsOffDiet = await prisma.meal.count({
+      where: {
+        userId: String(request.user?.id),
+        isOnDiet: false
+      }
+    })
+
+    const { longestSequenceOnDiet } = totalMealsRegistered.reduce((acc, meal) => {
+      if (meal.isOnDiet) {
+        acc.currentSequence += 1
+      } else {
+        acc.currentSequence = 0
+      }
+
+      if (acc.currentSequence > acc.longestSequenceOnDiet) {
+        acc.longestSequenceOnDiet = acc.currentSequence
+      }
+
+      return acc
+    }, { longestSequenceOnDiet: 0, currentSequence: 0 })
+
+    return {
+      totalMealsRegistered: totalMealsRegistered.length,
+      totalMealsOnDiet,
+      totalMealsOffDiet,
+      longestSequenceOnDiet
+    }
   })
 }
